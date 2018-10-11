94f262e2fc20cb43d75f7414de10f6f1346a85df
Tobias Valbjørn <tvalbjoern@gmail.com>
2018-10-11 20:57:01 +0200

    After 10 consecutive beats close to each other, the test will automatically initiate. It will start to calculate an average resting heart rate. When the test is done a function will be called, where the publishing to thingsspeak can be implemented. Afterwards the test is repeated. It is tested with wrist HR monitor, and it is precise. Indication of when the test begins and ends needs to be indicated, maybe with the motor

--- pulseIOT/src/pulseIOT.ino
+++ pulseIOT/src/pulseIOT.ino
@@ -37,48 +37,109 @@ In order for this app to compile correctly, the following Partible Build (Web ID
 
 */
 #include <SparkIntervalTimer.h>
+#include <SimpleTimer.h>
 
 extern void interruptSetup(void);
-
-extern int pulsePin;
 //Set to onboard LED D7
 extern int blinkPin;
+extern int pulsePin;
+
 extern volatile int BPM;;
 extern volatile int rawSignal;;
 extern volatile int IBI;
+
 extern volatile boolean Pulse;
 extern volatile boolean QS;
 
 //Pin that can be used to do fading with LED.
 //It is set to D6
 extern int fadePin;
-//int test=3;
 extern int fadeRate;
 
+int lastBPM=0;
+int consecBeat=0;
+int threshold=10;
+int accBeats=0;
+int numBeat=0;
+int avgBPM=0;
+unsigned long int timerDone=0;
+
 void setup(){
+	//Default fade pin is D6, but D0-D3 is PWM on particle photon.
 	fadePin=D3;
+
 	pinMode(blinkPin,OUTPUT);         // pin that will blink to your heartbeat!
 	pinMode(fadePin,OUTPUT);          // pin that will fade to your heartbeat!
 	Serial.begin(115200);             // we agree to talk fast!
 	interruptSetup();                 // sets up to read Pulse Sensor signal every 2mS
 }
 
-//  Where the Magic Happens
 void loop(){
-	if (QS == true){     //  A Heartbeat Was Found
-		// BPM and IBI have been Determined
-		// Quantified Self "QS" true when arduino finds a heartbeat
-		digitalWrite(blinkPin,HIGH);     // Blink LED, we got a beat.
-		fadeRate = 255;         // Makes the LED Fade Effect Happen
+
+	// BPM and IBI have been Determined
+	// Quantified Self "QS" true when arduino finds a heartbeat
+	if (QS == true){
+
+		//I want to avoid measuring big shifts in the heartbeats.
+		//I want to measure the heartbeat when the resting heart rate has settled.
+		if(abs(BPM-lastBPM) < 5)
+		{
+			consecBeat++;
+			//It can take some time to find the pulse. Sometimes a lucky few
+			//heartbeats can increase the consecBeat, but when consecBeat
+			//has reached the threshold, there is a high chance that the pulse happens
+			//been found and is steady, ready to meassure.
+			if(consecBeat==threshold){
+				Serial.println("Success! consecBeat is:");
+				Serial.println(consecBeat);
+				numBeat=0;
+				avgBPM=BPM;
+				timerDone=millis()+10000;
+				Serial.println("Timer set");
+			}
+			//we will only do the measurements when we are above the threshold
+			//for consequtive heartbeats, this will increase accuracy of data.
+			if(consecBeat>threshold){
+				numBeat++;
+				//accumulated beats for averaging
+				accBeats+=BPM;
+				avgBPM=accBeats/numBeat;
+			}
+		}
+		else{
+			//If you have found your pulse but lost it again, irregular beats can
+			//happen when you try to find it again. It will help avoid corrupting data
+			consecBeat--;
+		}
+
+		// Blink LED, we got a beat.
+		digitalWrite(blinkPin,HIGH);
+
 		// Set 'fadeRate' Variable to 255 to fade LED with pulse
-		serialOutputWhenBeatHappens();   // A Beat Happened, Output that to serial.
-		QS = false;                      // reset the Quantified Self flag for next time
+		fadeRate = 255;
+
+		//Output beat to serial.
+		serialOutputWhenBeatHappens();
+
+		// reset the Quantified Self flag for next time
+		QS = false;
 	}
 	else {
-		digitalWrite(blinkPin,LOW);            // There is not beat, turn off onboard LED
+		// There is not beat
+		digitalWrite(blinkPin,LOW);
 	}
-	ledFadeToBeat();                      // Makes the LED Fade Effect Happen
-	delay(20);                             //  take a break
+
+	ledFadeToBeat();
+	lastBPM=BPM;
+
+	if(millis()>=timerDone && consecBeat>=threshold)
+	{
+		sendToCloud();
+		Serial.println("Data send to cloud");
+		//Calculate the next time that data needs to be sent to cloud
+		timerDone=millis()+10000;
+	}
+	delay(20);
 }
 
 //  Decides How To OutPut BPM and IBI Data
@@ -89,9 +150,13 @@ void serialOutputWhenBeatHappens(){
 		Serial.print(BPM);
 		Serial.println("  ");
 }
-
 void ledFadeToBeat(){
 	fadeRate -= 15;                         //  set LED fade value
 	fadeRate = constrain(fadeRate,0,255);   //  keep LED fade value from going into negative numbers!
 	analogWrite(fadePin,fadeRate);          //  fade LED
+}
+
+void sendToCloud() {
+    Serial.println("Sending number of beats to cloud:");
+		Serial.println(avgBPM);
 }
